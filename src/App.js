69158725838e9cea15d24060f33ba71885ae85af
premjitheisnam <hpremjit7@gmail.com>
2020-11-28 23:47:16 +0530

    App js updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+
+import Navigation from "./Component/Navigation";
 
 class App extends Component {
-  render () {
-  return (
-    <div className="App">
-      hi i am react
-    </div>
-  );
-}
+  render() {
+    return (
+      <div className="App">
+        <Navigation />
+        hi i am react developer
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Component/Navigation.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const navigation = (props) => {
+  return <nav>Menu</nav>;
+};
+
+export default navigation;
