55720e70367e8ca8c17b6d99f82de3327836b5fd
premjitheisnam <hpremjit7@gmail.com>
2020-11-29 00:20:57 +0530

    layout imported

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import "./App.css";
 
 import Navigation from "./Component/Navigation";
+import Layout from "./Layout/Layout";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navigation />
+        <Layout />
         hi i am react developer
       </div>
     );
