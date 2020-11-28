cbeaf6649522f75e6fc18f95bc0904dac5756fb0
premjitheisnam <hpremjit7@gmail.com>
2020-11-28 23:15:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render () {
+  return (
+    <div className="App">
+      hi i am react
+    </div>
+  );
+}
+}
+
+export default App;
